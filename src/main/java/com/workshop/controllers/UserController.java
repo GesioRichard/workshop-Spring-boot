a1b84e8df087ad5eb331c6acc4bbcbae9ff7d569
Gesio <gesiorichard2022@outlook.com>
2023-07-05 23:08:02 -0300

    user methods, insert, update and delete created

--- src/main/java/com/workshop/controllers/UserController.java
+++ src/main/java/com/workshop/controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.workshop.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workshop.entities.User;
 import com.workshop.service.UserService;
@@ -30,5 +36,26 @@ public class UserController {
 		List<User> users = service.findAll();
 		return ResponseEntity.ok().body(users);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){
+		user = service.insert(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(location).body(user);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User newUser){
+		User user = service.update(id, newUser);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 }
--- src/main/java/com/workshop/service/UserService.java
+++ src/main/java/com/workshop/service/UserService.java
@@ -24,4 +24,24 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
+	
+	public User insert(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User update(Long id, User newUser) {
+		User entity = userRepository.getReferenceById(id);
+		updateUser(entity, newUser);
+		return userRepository.save(entity);
+	}
+	
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	private void updateUser(User entity, User newUser) {
+		entity.setName(newUser.getName());
+		entity.setEmail(newUser.getEmail());
+		entity.setPhone(newUser.getPhone());
+	}
 }
